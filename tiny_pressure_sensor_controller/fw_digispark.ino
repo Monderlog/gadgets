02b64b546b2b1b6ddd24ccc7d4681f001a451348
Monderlog <monderlog@gmail.com>
2022-10-03 03:28:50 +0300

    update

--- tiny_pressure_sensor_controller/fw_digispark.ino
+++ tiny_pressure_sensor_controller/fw_digispark.ino
@@ -2,14 +2,18 @@
 
 // for LCD w/ GPIO MODIFIED for the ATtiny85
 #include <LiquidCrystal_I2C.h>
+// I2C Master lib for ATTinys which use USI - comment this out to use with standard arduinos
+#include <TinyWireM.h>
 // дельта для калибровки датчика в милливольтах
-#define DELTA_mV_Press 20
+#define DELTA_mV_Press 0
 // напряжение питания датчика в милливольтах
 //требуется измерить текущее и указать значение/2, требуется использовать делитель напряжения на резисторах R1=R2 (аналоговый пин имеет ограничение измеряемого напряжения 2500мВ)
 //!от значения напряжения и его стабильности сильно зависит точность измерений
-#define Input_mV 2500
+#define Input_mV 4980
 #define pin_Press_sensor 1
 #define SERIAL_BAUD 115200
+// одно из стандартных значений, определить можно через Digispark_Examples/i2cScanner
+#define GPIO_ADDR 0x27
 
 unsigned long timing=0;
   unsigned int Press_read = 0;
@@ -17,14 +21,18 @@ unsigned long timing=0;
 
 float Press_sensor=1;
 // адрес 16 символьныого дисплея на 2 строки
-LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_I2C lcd(GPIO_ADDR,16,2);
 
 void setup() {
 Serial.begin(SERIAL_BAUD);
+
    pinMode(pin_Press_sensor, INPUT);
-  lcd.setBacklight(0);
+
+  TinyWireM.begin();                    // initialize I2C lib - comment this out to use with standard arduinos
+  lcd.init();                           // initialize the lcd
+  lcd.backlight();                      // Print a message to the LCD.
   delay(100);
-  lcd.setBacklight(55);
+  lcd.backlight();
   lcd.clear();
   lcd.setCursor(0, 1);
   lcd.print("Pressure sensor test");
@@ -41,8 +49,9 @@ timing = millis();
 }
 }
 
-void Pressure_Data_read()
+void Pressure_Data_read() 
   {
+// количество считываний для усреднения
   const byte count_read = 10;
 // массив для хранения данных
   int Analog_data_read[count_read];
@@ -71,33 +80,33 @@ void Pressure_Data_read()
 // берем среднее значение из полученного массива
       Press_read = Analog_data_read[count_read/2];
 // масштабируем полученную величину в милливольты
-      Press_read = map(Press_read, 0, 4095, 0, Input_mV)-DELTA_mV_Press;
+      Press_read = map(Press_read, 0, 4980, 0, Input_mV)-DELTA_mV_Press;
   Serial.print(Press_read);
   Serial.print(" -> ");
 // берем значения из рабочего диапазона датчика давления
-  if ((Press_read >= 250)&&(Press_read <= 2250))
+  if ((Press_read >= 500)&&(Press_read <= 4980))
   {
 // масштабируем мВ в значения МПа (500/2(делитель на резисторах R1=R2=10кОМ) - верхний диапазон датчика давления для датчика 1 МПа)
-  Press_bar = (map(Press_read, 250, 2250, 0, 250));
+  Press_bar = (map(Press_read, 500, 4980, 0, 1000));
 // забрасываем значение в float
   Press_sensor = Press_bar*0.01;
   }
 // присваиваем граничные значения при выходе за пределы рабочего диапазона датчика давления
-  else if (Press_read < 250)
+  else if (Press_read < 500)
   {  Press_sensor = 0.00;  }
-  else if (Press_read > 2250)
+  else if (Press_read > 4980)
   {  Press_sensor = 10.0;  }
 Serial.println(Press_sensor);
 }
 
 void LCD_print()
 {
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0, 0);
   lcd.print("Voltage: ");
   lcd.print(Press_read);
   lcd.print(" mV  ");
-    lcd.setCursor(0, 2);
-  lcd.print("Pressure: ");
+    lcd.setCursor(0, 1);
+  lcd.print("Press.: ");
   lcd.print(Press_sensor);
   lcd.print(" bar ");
 }
---
+++ tiny_pressure_sensor_controller/fw_nano_2.ino
@@ -0,0 +1,127 @@
+#include <microDS18B20.h>
+#include <GyverOLED.h>
+
+#define DELTA_mV_Press 0
+// напряжение питания датчика в милливольтах
+//требуется измерить текущее и указать значение/2, требуется использовать делитель напряжения на резисторах R1=R2 (аналоговый пин имеет ограничение измеряемого напряжения 2500мВ)
+//!от значения напряжения и его стабильности сильно зависит точность измерений
+#define Input_mV 4920
+#define pin_Press_sensor A1
+#define SERIAL_BAUD 9600
+// Цифровые датчики температуры, безадресно 18b20 на D3, D5 и D8
+MicroDS18B20<3> T_system_sensor; // на плате
+MicroDS18B20<5> T_cold_water_sensor; // ХВС
+MicroDS18B20<8> T_hot_water_sensor; // ГВС
+
+GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+
+unsigned long last_time;
+unsigned int Press_read = 0;
+unsigned int Press_bar = 0;
+unsigned int T_system = 0;
+unsigned int T_cold_water = 0;
+unsigned int T_hot_water = 0;
+
+float Press_sensor=1;
+
+void setup(){
+  Serial.begin(SERIAL_BAUD);
+
+    pinMode(pin_Press_sensor, INPUT);
+
+  oled.init();        // инициализация
+  oled.clear();       // очистка
+  oled.setScale(3);   // масштаб текста (1..4)
+  oled.home();        // курсор в 0,0
+  delay(100);
+  oled.setScale(1);
+  oled.print("Температура и давление");
+  oled.clear();
+delay(2000);
+}
+
+void loop(){
+  // ансихронный таймер
+  if(millis()-last_time > 1000){
+  // запрос давления
+  Pressure_Data_read();
+  // запрос температуры
+  T_system_sensor.requestTemp();
+  T_cold_water_sensor.requestTemp();
+  T_hot_water_sensor.requestTemp();
+}
+
+void Temperature(){
+  // ДАТЧИК на плате
+  Serial.print("T-sys: ");
+  // Т на плате
+  if (T_system_sensor.readTemp()) Serial.println(T_system_sensor.getTemp());
+  else Serial.println("error");
+  // ДАТЧИК Т ХВС
+  Serial.print(", T-cold: ");
+  // Т ХВС
+  if (T_cold_water_sensor.readTemp()) Serial.println(T_cold_water_sensor.getTemp());
+  else Serial.println("error");
+  // ДАТЧИК Т ГВС
+  Serial.print(", T-hot: ");
+  // Т ГВС
+  if (T_hot_water_sensor.readTemp()) Serial.println(T_hot_water_sensor.getTemp());
+  else Serial.println("error");
+  }
+}
+
+void Pressure_Data_read(){
+  const byte count_read = 10;
+  int Analog_data_read[count_read];                              // массив для хранения данных
+      for (byte i = 0; i < count_read; i++){
+      Analog_data_read[i] = analogRead(pin_Press_sensor);     // считываем вход и помещаем величину в ячейки массива
+      delay(2);
+      }                                                       // сортируем массив по возрастанию значений в ячейках
+        int New_data = 0;                                     // временная переменная
+        for (byte i = 0; i < count_read; i++){
+          for (byte j = 0; j < count_read - 1; j++){
+              if (Analog_data_read[j] > Analog_data_read[j + 1]){    // упорядочиваем массив в порядке возрастания
+
+              New_data = Analog_data_read[j];
+              Analog_data_read[j] = Analog_data_read[j + 1];
+              Analog_data_read[j + 1] = New_data;
+              }
+            }
+          }
+      Press_read = Analog_data_read[count_read/2];             // берем среднее значение из полученного массива
+      Press_read = map(Press_read, 0, 4095, 0, Input_mV)-DELTA_mV_Press;            // масштабируем полученную величину в милливольты 
+  Serial.print(Press_read);
+  Serial.print(" -> ");
+
+  if ((Press_read >= 500)&&(Press_read <= 4500)){                // берем значения из рабочего диапазона датчика давления
+  Press_bar = (map(Press_read, 500, 4500, 0, 500));    // масштабируем милливольты в значения давления (500 - верхний диапазон датчика давления для датчика 0,5 МПа) 
+  Press_sensor = Press_bar*0.01;                         // переводим значение в float
+  }
+  else if (Press_read < 500){                             // присваиваем граничные значения при выходе за пределы рабочего диапазона датчика давления
+    Press_sensor = 0.00;  }
+  else if (Press_read > 4500){
+    Press_sensor = 10.0;  }
+Serial.println(Press_sensor);
+}
+
+void LCD_print(){
+    oled.setCursor(0, 0);
+  oled.print("Voltage: ");
+  oled.print(Press_read);
+  oled.print(" mV  ");
+    oled.setCursor(0, 1);
+  oled.print("Pressure: ");
+  oled.print(Press_sensor);
+  oled.print(" bar  ");
+    oled.setCursor(0, 2);
+  oled.print("Т в шкафу: ");
+  oled.print(T_system);
+  oled.print(" С°  ");
+    oled.setCursor(0, 3);
+  oled.print(" Т холодной воды: ");
+  oled.print(T_cold_water);
+  oled.print(" С° ");
+  oled.print(" Т горячей воды: ");
+  oled.print(T_hot_water);
+  oled.print(" С° ");
+}
